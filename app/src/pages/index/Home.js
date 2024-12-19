61b4793a18e2d79eb08aeac95c6affd696e5945e
d1ng0ls <4b4c4t3666@gmail.com>
2024-12-18 23:55:57 -0300

    agora foi tudo do index - eu acho

--- app/src/pages/index/Home.js
+++ app/src/pages/index/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
+import "jquery";
+import 'jquery-parallax.js'
 
 export default function Home() {
     return(
-        <div className="container-home">
+        <div className="container-home parallax-window" data-parallax="scroll" data-image-src="https://images6.alphacoders.com/136/thumb-1920-1367529.png">
             <h1>Minha vida. Meu blog</h1>
             <p>POR BEATRIZ HELBRI</p>
             <a href="/login">
